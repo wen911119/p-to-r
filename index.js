9b33d458e3938b5a49a9fdd9e4b64ab7795f561e
wen911119 <wen911119@gmail.com>
2019-05-20 15:26:15 +0800

    fix#undefine

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+let base = 750
+if (typeof $P_2_R_BASE$ !=='undefined' && $P_2_R_BASE$!=='undefined') {
+  base = parseInt($P_2_R_BASE$)
+}
+
 export default function(px) {
   if (typeof px === 'number') {
-    let base = parseInt($P_2_R_BASE$ || 750)
     return px && px / (base / 10) + "rem";
   }
   return px
